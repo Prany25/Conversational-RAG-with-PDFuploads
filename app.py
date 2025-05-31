1d5a5619637241a684501d6120805257b5dbacfc
Prany25 <118735414+Prany25@users.noreply.github.com>
2025-05-31 21:54:19 +0530

    Streamlit and groq setup

--- app.py
+++ app.py
@@ -10,9 +10,24 @@ from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagePlaceholder
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+import langchain_core.runnables.history import RunnableWithMessageHistory
 
 from dotenv import load_dotenv
 load_dotenv()
 
 os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
 embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+st.title("Conversational RAG with PDF uploads and chat history")
+st.write("Upload pdfs and chat with their content")
+
+api_key=st.text_input("Enter your GROQ API key", type="password")
+
+if api_key:
+    llm=ChatGroq(groq_api_key=api_key,model_name="distil-whisper-large-v3-en")
+
+    session_id=st.text_input("Session ID", value="default_session")
+
+    
