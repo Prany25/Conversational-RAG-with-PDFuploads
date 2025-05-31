6a6b28b762324130dc624474f9f53711d4b5d406
Prany25 <118735414+Prany25@users.noreply.github.com>
2025-05-31 22:51:19 +0530

    Final Changes to Conversational RAG

--- app.py
+++ app.py
@@ -3,16 +3,16 @@
 import os
 import streamlit as st
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchian.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagePlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-import langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -66,3 +66,52 @@ if api_key:
             ("human", "{input}"),
         ]
     )
+
+    history_aware_retriever=create_history_aware_retriever(llm,retriever,contextualize_q_system_prompt)
+
+    system_prompt=(
+        "You are an assistant for question-answering tasks."
+        "Use the following pieces of retrived content to answer the "
+        "question. If you don't know the answer, say that you don't know"
+        "Use three sentences maximum and keep the answer concise."
+        "\n\n"
+        "{context}"
+    )
+
+    qa_prompt=ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    question_answer_chain=create_stuff_documents_chain(llm,qa_prompt)
+    rag_chain=create_retrieval_chain(history_aware_retriever,question_answer_chain)
+
+    def get_session_history(session:str)->BaseChatMessageHistory:
+        if session_id not in st.session_state.store:
+            st.session_state.store[session_id]=ChatMessageHistory()
+        return st.session_state.store[session_id]
+    
+    conversational_rag_chain=RunnableWithMessageHistory(
+        rag_chain,get_session_history,
+        input_messages_key="input",
+        history_messages_key="chathistory",
+        output_messages_key="answer"
+    )
+
+    user_input=st.text_input("Your question:")
+    if user_input:
+        session_history=get_session_history(session_id)
+        response=conversational_rag_chain.invoke(
+            {"input":user_input},
+            config={
+                "configurable": {"session_id":session_id}
+            },
+        )
+        st.write(st.session_state.store)
+        st.success("Assistant:",response['answer'])
+        st.write("Chat History: ", session_history.messages)
+else:
+    st.warning("Please enter the Groq Api key")
